26a6d1b44874ef90744f5fd914736a6e81fe13a9
Divyangna Moudgil <157092539+DivyangnaMoudgil@users.noreply.github.com>
2024-02-14 11:36:35 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+a=2;
+b=8;
+sum=a+b;
+printf("Sum of Two variables is: %d",sum);
+printf("YOUR PROGRAM IS COMPLETED !");
+printf("A new statement");
+}
